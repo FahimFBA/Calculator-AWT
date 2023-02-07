edf8a744e1947940b475e196b1bbdc9956b49fcd
Αλεξάνδρα Μπαλλιου <alexandrampalliou02@gmail.com>
2023-02-07 16:56:42 +0200

    new fuctions added TextField Button and Panel changed to JTextField, JButton and JPanel

--- CalculatorWithGUI.java
+++ CalculatorWithGUI.java
@@ -1,33 +1,40 @@
+
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import static java.lang.Double.parseDouble;
 
 class CalculatorWithGUI extends Frame implements ActionListener {
 
-  TextField textInput;
-  Panel panel;
+  JTextField textInput;
+  JPanel panel;
 
-  String[] btnString = { "7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", "C", "0", "=", "÷" };
-  Button[] btn = new Button[16];
-  int num1 = 0, num2 = 0, result = 0;
+  String[] btnString = { "7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", "C", "0", "=", "÷", "^", "√", "%", "⌫"};
+
+  int n = 20; //number of buttons
+  JButton[] btn = new JButton[n];
+  double num1 = 0;
+  double num2 = 0;
+  String result = " ";
   char charSymbol;
 
   public CalculatorWithGUI() {
 
     Font f = new Font("MONOSPACED", Font.BOLD, 18);
 
-    textInput = new TextField(10);
+    textInput = new JTextField(10);
     textInput.setFont(f);
 
-    panel = new Panel();
+    panel = new JPanel();
 
     add(textInput, "North");
     add(panel, "Center");
 
-    panel.setLayout(new GridLayout(4, 4));
+    panel.setLayout(new GridLayout(5, 4));
 
-    for (int i = 0; i < 16; i++) {
+    for (int i = 0; i < n; i++) {
 
-      btn[i] = new Button(btnString[i]);
+      btn[i] = new JButton(btnString[i]);
       btn[i].setFont(f);
       btn[i].addActionListener(this);
       panel.add(btn[i]);
@@ -48,38 +55,72 @@ class CalculatorWithGUI extends Frame implements ActionListener {
     switch (str) {
       case "+" -> {
         charSymbol = '+';
-        num1 = Integer.parseInt(textInput.getText());
+        num1 = parseDouble(textInput.getText());
         textInput.setText("");
       }
       case "-" -> {
         charSymbol = '-';
-        num1 = Integer.parseInt(textInput.getText());
+        num1 = parseDouble(textInput.getText());
         textInput.setText("");
       }
       case "*" -> {
         charSymbol = '*';
-        num1 = Integer.parseInt(textInput.getText());
+        num1 = parseDouble(textInput.getText());
         textInput.setText("");
       }
       case "÷" -> {
         charSymbol = '÷';
-        num1 = Integer.parseInt(textInput.getText());
+        num1 = parseDouble(textInput.getText());
         textInput.setText("");
       }
-      case "=" -> {
-        num2 = Integer.parseInt(textInput.getText());
-        switch (charSymbol) {
-          case '+' -> result = num1 + num2;
-          case '-' -> result = num1 - num2;
-          case '*' -> result = num1 * num2;
-          case '÷' -> result = num1 / num2;
+      case "^"-> {
+        charSymbol = '^';
+        num1 = parseDouble(textInput.getText());
+        textInput.setText("");
+      }
+      case "√"-> {
+        charSymbol = '√';
+        textInput.setText("");
+      }
+      case "%" -> {
+        charSymbol = '%';
+        num1 = parseDouble(textInput.getText());
+        textInput.setText("");
+      }
+      case "⌫" -> {
+        String theText = textInput.getText();
+        if (theText.length() == 0) {
+          result = "";
+        } else {
+          result = theText.substring(0, theText.length()-1);
         }
         textInput.setText(result + "");
-        result = 0;
+        result = " ";
+      }
+      case "=" -> {
+        num2 = parseDouble(textInput.getText());
+        switch (charSymbol) {
+          case '+' -> result = String.valueOf(num1 + num2);
+          case '-' -> result = String.valueOf(num1 - num2);
+          case '*' -> result = String.valueOf(num1 * num2);
+          case '÷' -> result = String.valueOf(num1 / num2);
+          case '^' -> result = Double.toString(Math.pow(num1, num2));
+          case '√' -> result = Double.toString(Math.sqrt(num2));
+          case '%' -> {
+            if(num2 == 0){
+              result ="DIVISOR IS 0";
+            }
+            else {
+              result = String.valueOf(Math.floorMod((long) num1, (long) num2));
+            }
+          }
+        }
+        textInput.setText(result + "");
+        result = " ";
       }
       case "C" -> {
         textInput.setText("");
-        num1 = num2 = result = 0;
+        num1 = 0;
       }
       default -> textInput.setText(textInput.getText() + str);
     }
